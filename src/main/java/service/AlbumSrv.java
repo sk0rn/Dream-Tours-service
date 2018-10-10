1d61e40e04c7ed9314591f76a51e97f7df6c85bf
Brambrulet <brambrulet@gmail.com>
2018-10-10 18:07:35 +0300

    Чтение/запись файлов в/из альбомов

--- src/main/java/service/AlbumSrv.java
+++ src/main/java/service/AlbumSrv.java
@@ -1,51 +1,45 @@
 package service;
 
+import org.apache.commons.io.FileUtils;
 import org.apache.log4j.Logger;
-import service.iface.IAlbumSrv;
 import utils.config.ConfigReader;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.InputStream;
+import java.io.*;
 import java.nio.file.Paths;
 
 /**
  * Чтение/запись файлов из/в альбомы
  */
-public class AlbumSrv implements IAlbumSrv {
-    @Override
-    public InputStream readFile(String albumName, String fileName) {
+public final class AlbumSrv {
+    private AlbumSrv() {
+    }
+
+    public static InputStream readFile(String albumName, String fileName) {
         File file = Paths.get(ConfigReader.getAlbumsRoot(), albumName, fileName).toFile();
         if (file.exists()) {
             try {
                 return new FileInputStream(file);
             } catch (FileNotFoundException e) {
-                Logger.getLogger(AlbumSrv.class).error("Can't read file.", e);
+                Logger.getLogger(AlbumSrv.class).error("Can't read file " + file.getName(), e);
             }
-        } else {
-            return null;
         }
         return null;
     }
 
-    @Override
-    public boolean writeFile(InputStream stream, String albumName, String fileName) {
-//        if (stream != null) {
-//            File album = Paths.get(ConfigReader.getAlbumsRoot(), albumName).toFile();
-//            album.mkdirs();
-//            if (album.exists() && album.isDirectory()) {
-//                try {
-//                    OutputStream out = new FileOutputStream(Paths.get(ConfigReader.getAlbumsRoot(), albumName, fileName).toFile());
-//                } catch (FileNotFoundException e) {
-//                    Logger.getLogger(AlbumSrv.class).error("Can't read file.", e);
-//                    e.printStackTrace();
-//                }
-//                //Files.wr
-//            }
-//
-//
-//        }
+    public static boolean writeFile(InputStream stream, String albumName, String fileName) {
+        if (stream != null) {
+            File album = Paths.get(ConfigReader.getAlbumsRoot(), albumName).toFile();
+            File file = Paths.get(ConfigReader.getAlbumsRoot(), albumName, fileName).toFile();
+            album.mkdirs();
+            if (album.exists() && album.isDirectory()) {
+                try {
+                    FileUtils.copyInputStreamToFile(stream, file);
+                    return true;
+                } catch (IOException e) {
+                    Logger.getLogger(AlbumSrv.class).error("Can't write file " + file.getName(), e);
+                }
+            }
+        }
         return false;
     }
 }
