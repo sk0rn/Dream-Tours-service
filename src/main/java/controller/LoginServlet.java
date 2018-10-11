98c6923b8a40c7a83917a45b8875074f4930b300
sk0rn <hyperthrone@gmail.com>
2018-10-11 21:33:47 +1100

    Merge branch 'develop' of D:\dev_edu\STC13_HT\Lab02_DreamTours with conflicts.

--- src/main/java/controller/LoginServlet.java
+++ src/main/java/controller/LoginServlet.java
@@ -9,8 +9,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-import static constants.Consts.LOGIN;
-
 public class LoginServlet extends HttpServlet {
     private ILoginService loginService;
 
@@ -28,7 +26,7 @@ public class LoginServlet extends HttpServlet {
             req.getSession().invalidate();
         }
         // если уже залогинены
-        if (req.getSession().getAttribute(LOGIN) != null) {
+        if (req.getSession().getAttribute("login") != null) {
             resp.sendRedirect("/tours");
         } else {
             req.getRequestDispatcher("/jsp/index.jsp").forward(req, resp);
@@ -38,11 +36,11 @@ public class LoginServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
-        String login = req.getParameter(LOGIN);
+        String login = req.getParameter("login");
         String password = req.getParameter("password");
         if (loginService.checkAuth(login, password)) {
             Integer role = loginService.getRole(login);
-            req.getSession().setAttribute(LOGIN, login);
+            req.getSession().setAttribute("login", login);
             req.getSession().setAttribute("option", role);
             System.out.println(role);
             resp.sendRedirect("/tours");
@@ -51,4 +49,5 @@ public class LoginServlet extends HttpServlet {
             resp.sendRedirect("/?errorCode=wrongLogin");
         }
     }
+
 }
